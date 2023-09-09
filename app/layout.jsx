c144384177ca31bd05368f6332aa966efa2b9fbb
joi-lightyears <kunthanhdat@gmail.com>
2023-09-09 13:06:18 +0700

    fix font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
-import '../styles/globals.scss'
+import '@/styles/globals.scss'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 import { Inter } from 'next/font/google'
- 
+
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
