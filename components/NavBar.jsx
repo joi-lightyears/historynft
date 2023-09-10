e57b5e218de8fced8c92be4bc47eef2596f0a73f
joi-lightyears <kunthanhdat@gmail.com>
2023-09-10 15:35:36 +0700

    add ethereum and fix font

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -82,6 +82,16 @@ export default function NavBar() {
               <button className="signInBtn" onClick={handleConnectWallet} disabled={loading}>
                 {loading ? 'Connecting...' : textBtn}
               </button>
+              <svg width="26px" height="47px" viewBox="0 0 256 417" version="1.1" preserveAspectRatio="xMidYMid">
+                <g>
+                  <polygon fill="#b97429" points="127.9611 0 125.1661 9.5 125.1661 285.168 127.9611 287.958 255.9231 212.32"/>
+                  <polygon fill="#F4BA41" points="127.962 0 0 212.32 127.962 287.959 127.962 154.158"/>
+                  <polygon fill="#c58642" points="127.9611 312.1866 126.3861 314.1066 126.3861 412.3056 127.9611 416.9066 255.9991 236.5866"/>
+                  <polygon fill="#F4BA41" points="127.962 416.9052 127.962 312.1852 0 236.5852"/>
+                  <polygon fill="#E28B31" points="127.9611 287.9577 255.9211 212.3207 127.9611 154.1587"/>
+                  <polygon fill="#8f6538" points="0.0009 212.3208 127.9609 287.9578 127.9609 154.1588"/>
+                </g>
+              </svg>
             </div>
           </div>
         </div>
