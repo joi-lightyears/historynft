fc5d299b3da294b7cef757666728f85c15d7cf40
joi-lightyears <kunthanhdat@gmail.com>
2023-09-10 13:52:20 +0700

    fix font weight

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.scss'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ 
+  subsets: ['latin'],
+  weight: ["400", "500", "600", "700", "800", "900"],
+ })
 
 export const metadata = {
   title: 'NFT',
@@ -14,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <NavBar />
         {children}
         <Footer />
