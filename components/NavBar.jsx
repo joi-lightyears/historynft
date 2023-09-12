f121e4270160fdd9e7e40b30b60cd3efa318ff1d
joi-lightyears <kunthanhdat@gmail.com>
2023-09-12 12:26:36 +0700

    fix network

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -21,14 +21,15 @@ import {
   arbitrum,
   base,
   zora,
+  klaytn
+  
 } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [mainnet, arbitrum, klaytn],
   [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
   ]
 );
